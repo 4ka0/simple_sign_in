4ead2b631671e254f4acf35eb2e619b3769d402c
Jon Lee <39420293+4ka0@users.noreply.github.com>
2021-01-15 16:24:28 +0900

    Added view tests

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -5,13 +5,7 @@ from django.contrib.auth import get_user_model
 from accounts.models import CustomUser
 
 
-class TestRegisterView(SimpleTestCase):
-    """
-    class RegisterView(CreateView):
-        form_class = CustomUserCreationForm
-        success_url = reverse_lazy("login")
-        template_name = "registration/register.html"
-    """
+class TestRegisterView(TestCase):
 
     def test_view_url_exists_at_desired_location(self):
         response = self.client.get('/accounts/register/')
@@ -27,13 +21,6 @@ class TestRegisterView(SimpleTestCase):
 
 
 class TestCustomUserUpdateView(TestCase):
-    """
-    class CustomUserUpdateView(UpdateView):
-        model = CustomUser
-        form_class = CustomUserUpdateForm
-        success_url = reverse_lazy("home")
-        template_name = "registration/user_update_form.html"
-    """
 
     @classmethod
     def setUpTestData(cls):
@@ -59,3 +46,50 @@ class TestCustomUserUpdateView(TestCase):
         testuser = CustomUser.objects.get(id=1)
         response = self.client.get(reverse('user_update', args=[testuser.id]))
         self.assertTemplateUsed(response, 'registration/user_update_form.html')
+
+
+class TestHomePageContent(TestCase):
+
+    def test_home_page_content_when_user_signed_in(self):
+
+        testuser = CustomUser.objects.create(
+            username="johnlennon",
+            first_name="John",
+            last_name="Lennon",
+            email="john@lennon.com",
+            position="Songwriter"
+        )
+        testuser.set_password('wibble1234')
+        testuser.save()
+
+        login = self.client.login(username="johnlennon", password="wibble1234")
+        self.assertTrue(login)
+
+        response = self.client.get(reverse("home"))
+        self.assertContains(
+            response, "You are logged in as the following user.", 1
+        )
+        self.assertContains(response, "Username:", 1)
+        self.assertContains(response, "johnlennon", 1)
+        self.assertContains(response, "Name:", 1)
+        self.assertContains(response, "John Lennon", 1)
+        self.assertContains(response, "Position:", 1)
+        self.assertContains(response, "Songwriter", 1)
+        self.assertContains(response, "Email:", 1)
+        self.assertContains(response, "john@lennon.com", 1)
+        self.assertContains(response, "Date registered:", 1)
+        self.assertContains(response, "Last logged in:", 1)
+        self.assertNotContains(response, "You are not signed in.")
+
+    def test_home_page_content_when_user_not_signed_in(self):
+        response = self.client.get(reverse("home"))
+        self.assertContains(response, "You are not signed in.", 1)
+        self.assertNotContains(
+            response, "You are logged in as the following user."
+        )
+        self.assertNotContains(response, "Username:")
+        self.assertNotContains(response, "Name:")
+        self.assertNotContains(response, "Position:")
+        self.assertNotContains(response, "Email:")
+        self.assertNotContains(response, "Date registered:")
+        self.assertNotContains(response, "Last logged in:")
