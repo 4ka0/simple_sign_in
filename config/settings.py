fcdec6c92952544e07afb6765afe48bb3b04f8f5
Jon Lee <39420293+4ka0@users.noreply.github.com>
2021-01-20 11:25:38 +0900

    Black code formatting

--- config/settings.py
+++ config/settings.py
@@ -84,9 +84,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    "default": env.dj_db_url("DATABASE_URL")
-}
+DATABASES = {"default": env.dj_db_url("DATABASE_URL")}
 
 
 # Password validation
